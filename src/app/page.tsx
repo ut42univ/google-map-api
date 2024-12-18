b99ae6e5e4a4b1548bf63233fab9d5c2b28f3dcb
tmba <ut42.nu@gmail.com>
2024-12-18 11:15:08 +0900

    Google Mapsを使用する新しいアプリコンポーネントを追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,19 @@
-const Home = () => {
-  return <div>Home</div>;
-};
+"use client";
 
-export default Home;
+import { APIProvider, Map, Marker } from "@vis.gl/react-google-maps";
+
+function App() {
+  const API_KEY = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY as string;
+  const position = { lat: 53.54992, lng: 10.00678 };
+  return (
+    <div style={{ height: "100vh", width: "100%" }}>
+      <APIProvider apiKey={API_KEY}>
+        <Map defaultCenter={position} defaultZoom={10}>
+          <Marker position={position} />
+        </Map>
+      </APIProvider>
+    </div>
+  );
+}
+
+export default App;
