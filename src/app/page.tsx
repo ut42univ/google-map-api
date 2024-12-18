ae6d6e08cd357160c5bc71d8c1755048524ec80a
tmba <ut42.nu@gmail.com>
2024-12-18 11:39:21 +0900

    地図の中心位置を東京都へ変更し、ズームレベルを調整

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,11 @@ import { APIProvider, Map, Marker } from "@vis.gl/react-google-maps";
 
 function App() {
   const API_KEY = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY as string;
-  const position = { lat: 53.54992, lng: 10.00678 };
+  const position = { lat: 35.67929, lng: 139.76901 };
   return (
-    <div style={{ height: "100vh", width: "100%" }}>
+    <div className="h-screen w-full">
       <APIProvider apiKey={API_KEY}>
-        <Map defaultCenter={position} defaultZoom={10}>
+        <Map defaultCenter={position} defaultZoom={7}>
           <Marker position={position} />
         </Map>
       </APIProvider>
